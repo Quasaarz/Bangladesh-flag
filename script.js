4d0eff9464ac48b3c097328025e197c9dfafbf70
Quasaarz <63438816+Quasaarz@users.noreply.github.com>
2020-10-07 18:20:24 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function can() {
- var canvas = document.getElementById("surface");
- if (canvas.getContext){
-  var con = canvas.getContext('2d');
-  con.fillStyle = "rgb(255,255,0)";
- con.fillRect(40,140,150,50);
- } //end if
-}//end draw
